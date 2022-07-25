3c70b4890edc128c6b4853d87b3a16a44e2e34aa
shuamp <castel.davila@gmail.com>
2022-07-25 10:53:55 +0100

    Add project files.

---
+++ CSproject/Program.cs
@@ -0,0 +1,36 @@
+﻿
+class Staff
+{   //fields
+    private float hourlyRate;
+    private int hWorked;
+
+    // auto-implemented properties
+    public float TotalPay { get;protected set; }
+    public float BasicPay { get;private set; }
+    public string? NameOfStaff { get; private set; }
+    public int HoursWorked
+    { 
+    get
+        {
+            return hWorked;
+        }
+    set
+        {
+            if(value > 0)
+            {
+                hWorked = value;
+            } else
+            {
+                hWorked = 0;
+            }
+        }
+    
+    }
+    //constructor
+    public Staff (string name, float rate)
+    {
+        NameOfStaff = name;
+        hourlyRate = rate;
+    }
+}
+
